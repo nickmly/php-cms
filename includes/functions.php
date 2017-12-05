bedc54e2a0db7375fd1fdaae049ed27ce4a65d77
nickmly <nickmly@gmail.com>
2017-12-04 22:14:52 -0500

    Search bar working

--- includes/functions.php
+++ includes/functions.php
@@ -41,4 +41,49 @@
         <?php
         }
     }
+
+    function displayPosts($posts) {
+        while($row = mysqli_fetch_assoc($posts)){
+            
+            $title = $row["post_title"];
+            $author = $row["post_author"];
+            $content = $row["post_content"];
+            $date = $row["post_date"];
+            $image = $row["post_image"];
+            ?>
+           
+            <h2>
+                <a href="#"><?php echo $title; ?></a>
+            </h2>
+            <p class="lead">
+                by <a href="index.php"><?php echo $author; ?></a>
+            </p>
+            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $date; ?></p>
+            <hr>
+            <img class="img-responsive" src="<?php echo $image; ?>" alt="">
+            <hr>
+            <p><?php echo $content; ?></p>
+            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+            <hr>
+        <?php
+        }
+    }
+
+    function searchForPosts() {
+        global $db_connect;        
+        if(isset($_POST['submit'])) {
+            $search = $_POST['search'];
+            // Select all post_tags similiar to search term
+            $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search' ";
+            $result = mysqli_query($db_connect, $query);
+            if(!$result) {
+                die("Search query failed! " . mysqli_error($db_connect));
+            }
+            $count = mysqli_num_rows($result);
+            echo "<h3>Results found: " . $count . "</h3>";
+            if($count > 0)
+                displayPosts($result);
+        }
+    }
 ?>
---
+++ search.php
@@ -0,0 +1,60 @@
+<?php 
+    include "includes/db.php";
+    include "includes/header.php";
+    include "includes/nav.php";
+    include_once "includes/functions.php";
+?>
+
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+            <h1 class="page-header">
+                Page Heading
+                <small>Secondary Text</small>
+            </h1>
+                        
+            <?php
+                searchForPosts(); 
+            ?>
+            <!-- Pager -->
+            <ul class="pager">
+                <li class="previous">
+                    <a href="#">&larr; Older</a>
+                </li>
+                <li class="next">
+                    <a href="#">Newer &rarr;</a>
+                </li>
+            </ul>
+
+        </div>
+        <!-- Blog Sidebar Widgets Column -->
+        <?php include "includes/sidebar.php"; ?>
+        
+
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Your Website 2014</p>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+    </footer>
+
+</div>
+<!-- /.container -->
+
+<?php
+    include "includes/footer.php";
+?>
